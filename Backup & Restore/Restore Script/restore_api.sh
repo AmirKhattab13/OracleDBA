9fec91bb10ce8dda29b8a0a7689599dd712e8318
AmirKhattab <126305696+AmirKhattab13@users.noreply.github.com>
2024-08-21 15:21:28 +0300

    Add files via upload

---
+++ Backup & Restore/Restore Script/restore_api.sh
@@ -0,0 +1,1 @@
+rman target /  cmdfile='/home/oracle/restore_api.rman' log='/home/oracle/restore_api.log'
---
+++ Backup & Restore/Restore Script/restore_cg .sh
@@ -0,0 +1,1 @@
+rman target /  cmdfile='/home/oracle/restore_cg.rman' log='/home/oracle/restore_cg.log'
